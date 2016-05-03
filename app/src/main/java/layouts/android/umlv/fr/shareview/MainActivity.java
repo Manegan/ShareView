c474d973a6986bdfb68de9ff0d52a47bcc5e92c6
Manegan <iskandaroff.yenrick@gmail.com>
2016-05-03 11:04:51 +0200

    Initial commit

---
+++ app/src/main/java/layouts/android/umlv/fr/shareview/MainActivity.java
@@ -0,0 +1,13 @@
+package layouts.android.umlv.fr.shareview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
