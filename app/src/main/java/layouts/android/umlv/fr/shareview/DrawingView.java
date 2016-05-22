ba726b8f70a2c17303d9ced627db5184af9bd4fe
Manegan <iskandaroff.yenrick@gmail.com>
2016-05-22 20:22:29 +0200

    blabla

--- app/src/main/java/layouts/android/umlv/fr/shareview/DrawingView.java
+++ app/src/main/java/layouts/android/umlv/fr/shareview/DrawingView.java
@@ -5,22 +5,21 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
-import android.util.TypedValue;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class DrawingView extends View {
-    private Path drawPath;
+public class DrawingView extends View{
+    private String selectedTool;
     private Paint drawPaint, canvasPaint;
     private int paintColor = 0xFF660000;
     private Canvas drawCanvas;
     private Bitmap canvasBitmap;
-    private float brushSize, lastBrushSize;
-    private boolean erase=false;
+    private int startingX = 0;
+    private int startingY = 0;
+
 
     public DrawingView(Context context) {
         super(context);
@@ -37,9 +36,12 @@ public class DrawingView extends View {
         setupDrawing();
     }
 
+    public void setSelectedTool(String selectedTool) {
+        this.selectedTool = selectedTool;
+        Log.d(getClass().getName(), "Selected Tool : "+this.selectedTool);
+    }
 
     public void setupDrawing() {
-        drawPath = new Path();
         drawPaint = new Paint();
 
         drawPaint.setColor(paintColor);
@@ -50,25 +52,17 @@ public class DrawingView extends View {
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        brushSize = getResources().getInteger(R.integer.medium_size);
-        lastBrushSize = brushSize;
-
         canvasPaint = new Paint(Paint.DITHER_FLAG);
-    }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvasBitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
         drawCanvas = new Canvas(canvasBitmap);
-        drawPaint.setStrokeWidth(brushSize);
+        selectedTool = "Line";
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
     }
 
     @Override
@@ -78,15 +72,31 @@ public class DrawingView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                drawPath.moveTo(touchX, touchY);
+                startingX = Math.round(touchX);
+                startingY = Math.round(touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                drawPath.lineTo(touchX, touchY);
+                // Do things for polyline, but eh, do we realize this feature ?
                 break;
             case MotionEvent.ACTION_UP:
-                drawCanvas.drawPath(drawPath, drawPaint);
-                drawPath.reset();
-                Paint paint = new Paint();
+                Log.v(getClass().getName(), "Drawing something with : "+ selectedTool);
+                switch (selectedTool) {
+                    case "Line":
+                        // TODO : new Line(), send it to server
+                        drawCanvas.drawLine(startingX, startingY, touchX, touchY,drawPaint);
+                        break;
+                    case "Rectangle":
+                        // TODO : new Rectangle(), send it to server
+                        drawCanvas.clipRect(startingX, startingY, touchX, touchY);
+                        break;
+                    case "Ellipsis":
+                        // TODO : new Ellipsis(), send it to server
+                        int radius = 10; // TODO : compute radius with touchX and touchY
+                        drawCanvas.drawCircle(startingX, startingY, radius, drawPaint);
+                        break;
+                    default:
+                        Log.v(getClass().getName(), "Unknown tool");
+                }
                 break;
             default:
                 return false;
@@ -101,29 +111,9 @@ public class DrawingView extends View {
         drawPaint.setColor(paintColor);
     }
 
-    public void setBrushSize(float newSize) {
-        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                newSize, getResources().getDisplayMetrics());
-        brushSize=pixelAmount;
-        drawPaint.setStrokeWidth(brushSize);
-    }
-
-    public void setLastBrushSize(float lastSize){
-        lastBrushSize=lastSize;
-    }
-
-    public float getLastBrushSize(){
-        return lastBrushSize;
-    }
-
-    public void setErase(boolean isErase) {
-        erase = isErase;
-        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        else drawPaint.setXfermode(null);
-    }
-
     public void startNew(){
         drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
         invalidate();
     }
+
 }
